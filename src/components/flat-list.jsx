0c56a09c3ac93175549e30b7beffbc561c782e6f
Ryan Woods <contact@ryanofwoods.com>
2021-03-31 18:52:34 +0200

    refactor code

--- src/components/flat-list.jsx
+++ src/components/flat-list.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import Flat from './flat';
 
@@ -8,7 +7,18 @@ const FlatList = ({ flats, selectFunction }) => {
       {
         flats.map((flat) => {
           const { name, imageUrl, price, priceCurrency, lat, lng } = flat;
-          return <Flat key={imageUrl} name={name} imageUrl={imageUrl} price={price} priceCurrency={priceCurrency} lat={lat} lng={lng} selectFunction={selectFunction} />;
+          return (
+            <Flat
+              key={imageUrl}
+              name={name}
+              imageUrl={imageUrl}
+              price={price}
+              priceCurrency={priceCurrency}
+              lat={lat}
+              lng={lng}
+              selectFunction={selectFunction}
+            />
+          );
         })
       }
     </div>
--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -14,12 +14,11 @@ class Flat extends Component {
       backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url("${imageUrl}")`
     };
     return (
-      <div className="card card-flat" style={flatStyle} alt="" onClick={this.handleClick}>
+      <div className="card" style={flatStyle} alt="" onClick={this.handleClick}>
         <div className="card-category">{`${price} ${priceCurrency}`}</div>
         <div className="card-description">
           <h2>{name}</h2>
         </div>
-        <a class="card-link" href="#"></a>
       </div>
     );
   }
